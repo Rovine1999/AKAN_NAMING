569a9f9db6d5c20ca5fbd5500090a42518391e0b
Rovine1999 <rovinewanjala99@gmail.com>
2020-10-07 21:10:47 +0300

    added

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const weekDays=["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
+const maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const femaleName=["Akosua", "Adwoa", "Abena", "Akua", "Yaa", "Afua","ama"];
